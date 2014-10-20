61c90bf54013e3b7cb5604cb0a9a19ba3ba2477c
Rishi Dhanaraj <rishid97@gmail.com>
2014-10-19 21:56:38 -0700

    Make assignments movies

--- assignments/script.js
+++ assignments/script.js
@@ -1,66 +1,58 @@
-function movie(img_url, genre, rating, title, description) {
-  this.img_url = img_url;
-  this.description = description;
-  this.genre = genre;
-  this.rating = rating;
+function movie(title, rating, price, img_url, description) {
+
   this.title = title;
+  this.rating = rating;
+  this.price = price;
+  this.img_url = img_url;
 
   this.display = display;
 }
 
-function display () {
-  document.write("<div class='movies'>");
-  document.write("<div class='img'><img src='" + this.img_url + "'></div>")
-  document.write("<div class='attr'><h4>" + this.title + "</h4>");
-  document.write("<strong><p>Genre</strong>: " + this.genre + "</p>")
-      
-  document.write("<strong><p>Rating</strong>: " + this.rating + "</p>")
-  document.write("<strong><p>Description</strong>: " + this.description + "</p></div></div>");
+function display() {
+  document.write("<td><img src='" + this.img_url + "'></img>");
+  document.write("<td>" + this.title + "</td>");
+  document.write("<td>" + this.rating + "</td>");
+  document.write("<td>$" + this.price + "</td>");
 }
 
-function movie1() {
-  movie = new movie("http://content6.flixster.com/movie/11/17/69/11176976_det.jpg", 
-  "Comedy", "R", "Wolf of Wall Street",
-  "The Wolf of Wall Street is a 2013 American black comedy film directed by " +
-  "Martin Scorsese, based on Jordan Belfort's memoir of the same name.");
+function movies() {
+  comedy = [], action = [], drama = [];
+  catalog = [comedy, action, drama];
 
-  movie.display();
-}
+  comedy.title = "Comedy", action.title = "Action", drama.title = "Drama";
+  catalog.displayAll = function() {
+    document.write("<table>");
 
-function movie2() {
-  movie = new movie("http://content6.flixster.com/movie/11/15/21/11152124_det.gif", 
-  "Documentary", "PG-13", "The Social Network",
-  "Adapted from Ben Mezrich's 2009 book, the film portrays the founding of social networking website Facebook and the resulting lawsuits.");
+    for (var i = 0; i < catalog.length; i++) {
+      genre = catalog[i];
 
-  movie.display();
-}
+      for (var j = 0; j < genre.length; j++) {
+        if(j == 0)
+          document.write("<tr><td><strong>" + genre.title + "</strong></td>");
+        else
+          document.write("<tr><td></td>");
 
-
-function movie3() {
-  movie = new movie("http://content8.flixster.com/movie/11/16/80/11168038_det.jpg", 
-  "Action", "PG-13", "Men in Black 3",
-  "Men in Black 3 is a 2012 American 3D science fiction action comedy film directed by Barry Sonnenfeld and starring Will Smith and Josh Brolin.");
-
-  movie.display();
-}
-
-function selectProduct() {
-  document.write("  <link href='http://fonts.googleapis.com/css?family=Oxygen+Mono' rel='stylesheet' type='text/css'> <link rel='stylesheet' type='text/css' href='style.css'>");
-  document.write("<div class='nav'><a href='' id='brand'>Rotten Potatoes</a><span class='text-right'><a href='index.html'>Movies </a>&nbsp;<a href='#'>Books</a></span><hr></div>")
-
-  var first = prompt("Enter your first name.")
-  var last = prompt("And your last name?");
-
-  alert("Rotten Potatoes is a place for book and movie reviews.\n\nWelcome to the site, " + first + " " + last + "!");
-
-  last = prompt("Movies showing within five miles (enter the item number)\n\n\t1. Wolf of Wall Street\n\t2. Social Network\n\t3. Men in Black 3\n");
-
-  switch(last) {
-    case "1": movie1(); break;
-    case "2": movie2(); break;
-    case "3": movie3(); break;
-    default: 
-      document.write("<div class='hero-unit'>Error, oh no! Did you enter a number besides 1, 2, or 3?");
-      document.write("<br><br><a href='index.html'>Return</a></div>")
+        genre[j].display();
+        document.write("</tr>");
+      };
+    };
+    document.write("</table>");
   }
+
+  movie1 = new movie("Hotel Transylvania", "PG", 14.99, "http://goo.gl/l1j3Bw");
+  movie2 = new movie("Ted", "R", 11.99, "http://goo.gl/W8fjFc");
+  movie3 = new movie("Madagascar", "PG", 12.99, "http://goo.gl/Wuayk8");
+  comedy.push(movie1, movie2, movie3);
+
+  movie4 = new movie("The Avengers", "PG-13", 12.99, "http://goo.gl/hOxa2B");
+  movie5 = new movie("Men In Black 3", "PG-13", 10.99, "http://goo.gl/HxFSPL");
+  movie6 = new movie("The Hunger Games", "PG-13", 11.99, "http://goo.gl/u0z6eZ");
+  action.push(movie4, movie5, movie6);
+
+  movie7 = new movie("Taken 2", "PG-13", 13.99, "http://content8.flixster.com/movie/11/17/67/11176778_det.jpg");
+  movie8 = new movie("The Untouchables", "R", 12.99, "http://goo.gl/TNkFQY");
+  movie9 = new movie("Rock of Ages", "R", 11.99, "http://content9.flixster.com/movie/11/16/67/11166791_det.jpg");
+  drama.push(movie7, movie8, movie9);
+
+  catalog.displayAll();
 }
