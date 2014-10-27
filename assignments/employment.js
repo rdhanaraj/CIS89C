c852c9e05c8fe85be13746d8a6fb0aa2c18aea9a
Rishi Dhanaraj <rishid97@gmail.com>
2014-10-26 23:07:48 -0700

    Assignment update, yo

---
+++ assignments/employment.js
@@ -0,0 +1,17 @@
+function openPosition1() {
+  win1 = window.open("pos1.html", "", "height=300,width=500,resizable,scrollbars");
+}
+
+function openPosition2 () {
+  win2 = window.open("pos2.html", "", "height=300,width=500,resizable,scrollbars");
+}
+
+function printAndClosePage1() {
+  window.print();
+  window.close(); 
+}
+
+function printAndClosePage2() {
+  window.print();
+  window.close(); 
+}
